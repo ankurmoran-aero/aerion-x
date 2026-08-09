4c8384f266ba1eabcb50c373a77cce8576c0d70f
Ankur Moran <ankurmoran96@gmail.com>
2026-08-09 19:53:11 +0530

    chore: remove dev/temp file

--- patch_brahmos.py
+++
@@ -1,8 +0,0 @@
-with open("main.py", "r") as f:
-    content = f.read()
-
-content = content.replace("BrahMos Cloud", "Aerion-X Net")
-content = content.replace("Terminate connection to Aerion-X Net", "Terminate connection to Aerion-X")
-
-with open("main.py", "w") as f:
-    f.write(content)
